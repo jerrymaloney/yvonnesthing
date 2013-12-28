d5cd425655ab4bc6cdbdf5477d96d6e24a4c5618
jerrymaloney <gpjerrymaloney@gmail.com>
2013-12-28 18:28:03 +0000

    remove unnecessary steps from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,1 @@
 sudo puppet apply puppet/manifests/default.pp --modulepath puppet/modules
-sudo mkdir -p /usr/local/lib/node_modules/yvonnesthing
-sudo mkdir -p /usr/local/lib/node_modules/yvonnesthing
-sudo cp src/* /usr/local/lib/node_modules/yvonnesthing
-sudo service yvonnesthing restart
